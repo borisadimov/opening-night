56729b5114b2a48de00442304598c4ff757ccc66
Boris Adimov <boris@adimov.ru>
2016-10-25 11:13:16 +0300

    push to prod

--- build/clean-up-build.js
+++ build/clean-up-build.js
@@ -9,6 +9,7 @@ const webpackConfig = require('./webpack.prod.conf');
 let spinner = ora('cleaning production build');
 spinner.start();
 
+let oldPath = path.resolve(__dirname)
 let destPath = path.resolve(__dirname, '../dist');
 
 replace({
@@ -28,6 +29,18 @@ replace({
   replacement: `id="app" class="loading">`,
   paths: [destPath+'/index.html']
 });
+
+cd(destPath);
+
+rm('-rf', '.git');
+exec('git init .');
+exec("git add -A");
+exec("git commit -m 'updated static views'");
+exec("git remote add origin git@github.com:opening-night/opening-night.github.io.git");
+exec("git push -f --set-upstream origin master");
+
+cd(oldPath);
+
 
 
 spinner.stop();
